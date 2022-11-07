5335bb6e16a6f8b70f01cc9eacad0d05c4b3d6bc
root <root@LAPTOP-AM0O76AV.localdomain>
2022-11-07 22:45:21 +0800

    fix: bug

--- func.py
+++ func.py
@@ -1,6 +1,18 @@
 def get_replay_content(recContent, user):
-    print(user)
+    res = ""
     internal = False
     if user == "oS7PY6UQHZiy1HvydBL4sLAaiBgI":
         internal = True
-    return internal
+    if recContent == "":
+        pass
+    elif recContent == "":
+        pass
+    elif recContent == "":
+        pass
+    elif recContent == "":
+        pass
+    elif recContent == "":
+        pass
+    else:
+        res = "暂不支持此功能"
+    return res
