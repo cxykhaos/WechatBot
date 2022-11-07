3bc3a077d44caeae939c6b744e5b0c630580bbb7
root <root@LAPTOP-AM0O76AV.localdomain>
2022-11-07 22:22:05 +0800

    fix: bug

--- handle.py
+++ handle.py
@@ -4,15 +4,15 @@ import web
 import hashlib
 import receive
 import replay
-import requests
-import json
+import os
 
-botIp = "127.0.0.1" # 本机IP
-botPort = '5002'
+botIp = "127.0.0.1"  # 本机IP
+botPort = "5002"
+wxtoken = os.environ.get("WXTOKEN")
 
 
 class Handle(object):
-    #改get请求用于验证Token
+    # 改get请求用于验证Token
     def GET(self):
         try:
             data = web.input()
@@ -23,17 +23,17 @@ class Handle(object):
             timestamp = data.timestamp
             nonce = data.nonce
             echostr = data.echostr
-            token = "" #请按照公众平台官网\基本配置中信息填写
+            token = wxtoken
 
             li = [token, timestamp, nonce]
             li.sort()
-            tmp_str = "".join(li).encode('utf-8')
-            #进行sha1加密
+            tmp_str = "".join(li).encode("utf-8")
+            # 进行sha1加密
             hashcode = hashlib.sha1(tmp_str).hexdigest()
 
-            #sha1 = hashlib.sha1()
-            #map(sha1.update, li)
-            #hashcode = sha1.hexdigest()
+            # sha1 = hashlib.sha1()
+            # map(sha1.update, li)
+            # hashcode = sha1.hexdigest()
             print("handle/GET func: hashcode, signature: ", hashcode, signature)
             if hashcode == signature:
                 return echostr
@@ -41,20 +41,21 @@ class Handle(object):
                 return ""
         except Exception as Argument:
             return Argument
+
     def POST(self):
         try:
             webData = web.data()
-            print("Handle Post webdata is ", webData.decode('utf-8'))
-            #后台打日志
+            print("Handle Post webdata is ", webData.decode("utf-8"))
+            # 后台打日志
             recMsg = receive.parse_xml(webData)
-            # 该模块是处理文本数据信息 
-            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
-                #获取到并解析出来用户发送过来的数据信息
-                recContent = recMsg.Content.decode('utf-8')
-                print("user message:", recContent,type(recContent))
+            # 该模块是处理文本数据信息
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == "text":
+                # 获取到并解析出来用户发送过来的数据信息
+                recContent = recMsg.Content.decode("utf-8")
+                print("user message:", recContent, type(recContent))
                 # 获取到MsgID 作为信息唯一标识送入RASA中
                 msgID = recMsg.MsgId
-                print("user message ID :", msgID,type(recContent))
+                print("user message ID :", msgID, type(recContent))
                 replayContent = "QQ群：342950180"
                 print("------------")
                 print("bot recContent: ", replayContent)
